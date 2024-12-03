e46e9387c5c50265f1a9ec1e2469c9c6f9502127
dominick038 <dominickherweijer@gmail.com>
2024-12-03 08:40:24 +0100

    Day 3 pt 1 solution

--- Day3/main.pas
+++ Day3/main.pas
@@ -3,7 +3,8 @@ unit Main;
 interface
 
 uses
-  System.Classes;
+  System.Classes, 
+  System.Generics.Collections;
 
 type
   {$REGION 'Initialization'}
@@ -15,7 +16,7 @@ type
 
   TAOCSolution = class(TInterfacedObject, IAOCSolution)
   strict private
-
+    function  ExtractMul(const InputTxt: string): TList<TPair<Integer, Integer>>;
   public
     {$REGION 'Initialization'}
     class function  Solution: IAOCSolution;
@@ -26,11 +27,39 @@ type
 
 implementation
 
+uses
+  System.RegularExpressions, 
+  System.SysUtils;
+
 { TAOCSolution }
+
+function TAOCSolution.ExtractMul(const InputTxt: string): TList<TPair<Integer, Integer>>;
+const
+  RegexPattern = 'mul\(\d*,\d*\)';
+begin
+  Result := TList<TPair<Integer, Integer>>.Create;
+  var RegEx := TRegEx.Create(RegexPattern);
+
+  var Match := RegEx.Match(InputTxt);
+  var Count := 0;
+  while Match.Success do
+  begin
+    var Values := Match.Value.Split(['mul(', ',', ')']);
+    
+    Result.Add(TPair<Integer, Integer>.Create(StrToInt(Values[1]), StrToInt(Values[2])));
+
+    Match := Match.NextMatch;
+  end;
+end;
 
 procedure TAOCSolution.Solve(const Lines: TStringList);
 begin
+  var MultiplicationPairs := ExtractMul(Lines.Text);
+  var Total := 0;
+  for var Pair in MultiplicationPairs do
+    Total := Total + (Pair.Key * Pair.Value);
 
+  WriteLn(Total);
 end;
 
 {$REGION 'Initialization'}
